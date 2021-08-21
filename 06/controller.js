0f672bef1013c8f10913593523f8606078cea83a
yenilikci <m.melihcelik017@outlook.com>
2021-08-21 20:42:25 +0300

    controller ve module ayrı dosyalar - ders 6

---
+++ 06/controller.js
@@ -0,0 +1,8 @@
+app.controller("myControl", function ($scope) {
+    $scope.name = "MELİH";
+    $scope.surname = "ÇELİK";
+});
+
+app.controller("secondControl", function ($scope) {
+    $scope.age = 22;
+});
